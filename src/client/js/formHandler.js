f6ffcf6acd413bb0e7dfead1ec892852a7cb27f6
Nikita Ivanov <mybestjob@myself.com>
2021-02-09 16:52:44 +0300

    Fetch request to external API added

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -14,6 +14,34 @@ function handleSubmit(event) {
     .then(function(data) {
         document.getElementById('results').innerHTML = data.message
     })
+
+    console.log("::: External Fetch Request Sent:::")
+
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&zip=';
+    const key = '&APPID=36d7234802a84134f886ebad20d776d0';
+    const zip = '94016'
+    const res = getAPIData(baseURL,zip,key)
+        .then((weatherData)=>updateUI(weatherData));
 }
 
 export { handleSubmit }
+
+const getAPIData = async (baseURL='', zip='', key='') => {
+    const res = await fetch(baseURL+zip+key);
+    try{
+        const weatherData = await res.json();
+        return {
+            temperature: weatherData.main.temp,
+            date: new Date(weatherData.dt * 1000)
+        };
+    } catch(error) {
+        console.log('error', error);
+    }
+}
+
+const updateUI = async (weatherData) => {
+    const temperature = weatherData.temperature;
+    const date = weatherData.date;
+    document.getElementById('results').innerHTML = temperature;
+    return;
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,13 +20,18 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
     ]
 }
